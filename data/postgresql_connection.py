5c15715023536052a6e18568aee7a3441ba01176
Alexandr Kolos <saniakolos1@gmail.com>
2024-06-01 19:08:29 +0300

    added connection to database

---
+++ data/postgresql_connection.py
@@ -0,0 +1,61 @@
+from tkinter import messagebox
+import psycopg2
+
+
+# Клас для підключення до бази даних
+class Database:
+    def __init__(self, host='localhost', port=5432, dbname='City Electric Transport', user='postgres', password='3515'):
+        self.host = host
+        self.port = port
+        self.dbname = dbname
+        self.user = user
+        self.password = password
+        self.conn = None
+        self.cur = None
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.disconnect()
+
+    def connect(self):
+        try:
+            self.conn = psycopg2.connect(
+                dbname=self.dbname,
+                user=self.user,
+                password=self.password,
+                host=self.host,
+                port=self.port
+            )
+            self.cur = self.conn.cursor()
+        except Exception as e:
+            messagebox.showerror("Помилка", "Unable to connect to the database: " + str(e))
+
+    def disconnect(self):
+        if self.cur:
+            self.cur.close()
+        if self.conn:
+            self.conn.close()
+
+    def execute_query(self, query, params=None):
+        try:
+            if params:
+                self.cur.execute(query, params)
+            else:
+                self.cur.execute(query)
+            self.conn.commit()
+        except Exception as e:
+            messagebox.showerror("Помилка", str(e))
+
+    def fetch_data(self, query, params=None):
+        try:
+            if params:
+                self.cur.execute(query, params)
+            else:
+                self.cur.execute(query)
+            return self.cur.fetchall()
+        except Exception as e:
+            messagebox.showerror("Помилка", str(e))
+            return None
